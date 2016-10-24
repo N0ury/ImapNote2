0448deb35ddc005a76362933a1870cc6d5b57f9d
kwhitefoot <kwhitefoot@hotmail.com>
2016-10-24 12:29:23 +0200

    Added comments to make it easier to understand
    The process of adding comments to code that I read helps me make sense of it even if I never read them again.  Comments that are just a question mark are place holders to return to when I have a better idea of what that piece of code is for.

--- ImapNote2/src/main/java/com/Pau/ImapNotes2/ImapNotes2.java
+++ ImapNote2/src/main/java/com/Pau/ImapNotes2/ImapNotes2.java
@@ -9,6 +9,7 @@ import com.Pau.ImapNotes2.Miscs.OneNote;
 import android.app.Application;
 import android.content.Context;
 
+
 public class ImapNotes2 extends Application {
 	
 	private ConfigurationFile thisSessionConfigurationFile;
@@ -16,35 +17,44 @@ public class ImapNotes2 extends Application {
 	private ArrayList<OneNote> noteList;
         private static Context context;
 
+        // Called when starting the application.
         public void onCreate(){
             super.onCreate();
+            // Save the context in a static so that it is easy toi access everywhere.
             ImapNotes2.context = getApplicationContext();
         }
 
+        // Simplify access to the application context.
         public static Context getAppContext() {
             return ImapNotes2.context;
         }
 
+    // ?
 	public void SetConfigurationFile(ConfigurationFile currentSettings){
 		this.thisSessionConfigurationFile = currentSettings;
 	}
 	
+    // ?
 	public ConfigurationFile GetConfigurationFile(){
 		return this.thisSessionConfigurationFile;
 	}
 	
+    // ?
 	public void SetImaper(Imaper currentImaper){
 		this.thisSessionImapFolder = currentImaper;
 	}
 	
+    // ?
 	public Imaper GetImaper(){
 		return this.thisSessionImapFolder;
 	}
 	
+    // ?
 	public void SetNotesList(ArrayList<OneNote> currentNotesList){
 		this.noteList = currentNotesList;
 	}
 	
+    // ?
 	public ArrayList<OneNote> GetNotesList(){
 		return this.noteList;
 	}
